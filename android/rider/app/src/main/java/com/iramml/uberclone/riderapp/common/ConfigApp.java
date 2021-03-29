c8095c5368c65425a0ea1878ad029f8ca849a8a0
Ibrahim <shaikhibrahim7737@gmail.com>
2021-03-29 23:16:23 +0530

    init

--- android/rider/app/src/main/java/com/iramml/uberclone/riderapp/common/ConfigApp.java
+++ android/rider/app/src/main/java/com/iramml/uberclone/riderapp/common/ConfigApp.java
@@ -7,4 +7,5 @@ public class ConfigApp {
     public static final String CLOUD_MESSAGING_SERVER_GEY = "AAAAbpgjHto:APA91bF8d6cswpg89tSOpMtY0oqy3AqS0JOJdQ5LtAZ1l2soNEKdFdNuH3zsKwADQWUUKcliNo7Oc5JXSdJciWnLoEPdMIg3eVGei9LPMfDp2C4q6KXcpdBkdMSMFS7vthFhDDFFmgdd";
     public static final String GOOGLE_API_KEY = "AIzaSyBj1ibHylP9o0bS98ZS-XPSkobxvnZMDA8";
 
+
 }
